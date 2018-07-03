54c499b6212958c19a9a3414b0c40c9457bdbc34
Aster Valdemoro <astervaldemoro@gmail.com>
2018-07-02 20:50:27 -0400

    initial commit

---
+++ app/src/main/java/co/asterv/popularmoviesstage1/MovieDetails.java
@@ -0,0 +1,69 @@
+package co.asterv.popularmoviesstage1;
+
+import android.content.Intent;
+import co.asterv.popularmoviesstage1.model.*;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import co.asterv.popularmoviesstage1.model.Movie;
+import co.asterv.popularmoviesstage1.utils.JsonUtils;
+
+import static android.support.v4.app.NotificationCompat.getExtras;
+
+public class MovieDetails extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_movie_details);
+
+        TextView originalTitleTV = (TextView) findViewById (R.id.titleTextView);
+        TextView ratingTV = (TextView) findViewById (R.id.ratingTextView);
+        TextView releaseDateTV = (TextView) findViewById (R.id.releaseDateTextView);
+        TextView overviewTV = (TextView) findViewById (R.id.overviewTextView);
+        ImageView posterIV = (ImageView) findViewById (R.id.posterImageView);
+
+        Intent intent = getIntent();
+        if (intent == null) {
+            closeOnError();
+        }
+
+        Movie movie = intent.getParcelableExtra("movie");
+
+        // TITLE
+        originalTitleTV.setText(movie.getOriginalTitle());
+        // VOTER AVERAGE / RATING
+        ratingTV.setText (String.valueOf(movie.getVoterAverage ()) + " / 10");
+        // IMAGE
+        Picasso.with(this)
+                .load(movie.getPosterPath())
+                .fit()
+                .error(R.mipmap.ic_launcher_round)
+                .placeholder(R.mipmap.ic_launcher_round)
+                .into(posterIV);
+
+        // OVERVIEW
+        overviewTV.setText (movie.getOverview ());
+
+        // RELEASE DATE
+        releaseDateTV.setText (movie.getReleaseDate());
+    }
+
+
+    private void closeOnError() {
+        finish();
+        Toast.makeText(this, "Something went wrong.", Toast.LENGTH_SHORT).show();
+    }
+}
